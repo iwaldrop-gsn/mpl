5ae9b497b911157fd82b251e38009326beff7ea2
Ian Waldrop <iwaldrop@gsngames.com>
2021-07-02 13:19:04 -0700

    experiment with property accessor for MPLGlobalConfig

--- vars/MPLGlobalConfig.groovy
+++ vars/MPLGlobalConfig.groovy
@@ -1,5 +1,7 @@
 import com.griddynamics.devops.mpl.MPLManager
 
-def call() {
-	MPLManager.instance.globalConfig
-}
+def call() { MPLManager.instance.globalConfig }
+
+def propertyMissing(name) { call().(name) }
+
+//def propertyMissing(name, value) { call().(name) = value}
