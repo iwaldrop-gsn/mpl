9962cf88ea840543b6d6b3184c14f34c5861ba48
Ian Waldrop <iwaldrop@gsngames.com>
2021-07-02 13:39:54 -0700

    debugging

--- vars/MPLGlobalConfig.groovy
+++ vars/MPLGlobalConfig.groovy
@@ -2,6 +2,6 @@ import com.griddynamics.devops.mpl.MPLManager
 
 def call() { MPLManager.instance.globalConfig }
 
-def propertyMissing(name) { call().(name) }
+def propertyMissing(name) { call()[name] }
 
 //def propertyMissing(name, value) { call().(name) = value}
