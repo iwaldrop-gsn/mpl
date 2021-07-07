17e0cb94aae7f1a55632b67436f1f2caeba8c535
Ian Waldrop <iwaldrop@gsngames.com>
2021-07-06 17:56:23 -0700

    ibid

--- vars/MPLGlobalConfig.groovy
+++
@@ -1,8 +0,0 @@
-import com.griddynamics.devops.mpl.MPLManager
-
-def call() { MPLManager.instance.globalConfig }
-
-def propertyMissing(String name) {
-	echo "MPLGlobalConfig.propertyMissing: $name"
-	MPLManager.instance.globalConfig[name]
-}
