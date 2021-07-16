aa34a367cfb6a03da6e1a1a424d9636d7a565738
Ian Waldrop <iwaldrop@gsngames.com>
2021-07-16 12:57:55 -0700

    set current result to success after each module successfully runs

--- vars/MPLModule.groovy
+++ vars/MPLModule.groovy
@@ -81,6 +81,7 @@ def call(String name = env.STAGE_NAME, Map cfg = null) {
 	String block_id = MPLManager.instance.pushActiveModule(module_path)
 	try {
 		Helper.runModule(module_src, module_path, [CFG: cfg, OUT: out])
+		currentBuild.result = 'SUCCESS'
 	}
 	catch (FlowInterruptedException ex) {
 		// The exception is used by Jenkins to abort a running build and consequently
